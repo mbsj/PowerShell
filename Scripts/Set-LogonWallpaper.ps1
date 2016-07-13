5f039bf1bd0bd47da37131ad5889ea9b455c0010
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2016-07-13 11:21:16 +0200

    Moved files from KMD

---
+++ Scripts/Set-LogonWallpaper.ps1
@@ -0,0 +1,9 @@
+﻿Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background -Name OEMBackground -Value 1 -Type DWord
+
+if (-not (Test-Path "C:\Windows\System32\oobe\info\backgrounds")) {
+    New-Item "C:\Windows\System32\oobe\info\backgrounds" -ItemType Directory -Force
+}
+
+if (Test-Path "$env:USERPROFILE\Pictures\LogonScreen.jpg") {
+    Copy-Item -Path "$env:USERPROFILE\Pictures\LogonScreen.jpg" -Destination "C:\Windows\System32\oobe\info\backgrounds\backgroundDefault.jpg" -Force
+}
