63e8d401acd5f202b3267887775fe58ff3ef3d3a
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2016-08-01 21:25:05 +0200

    Expanded custom class resource and addded help

--- DSC/Custom Resource/ClassResource/Test-CreateClassResource.ps1
+++ DSC/Custom Resource/ClassResource/Test-CreateClassResource.ps1
@@ -9,5 +9,6 @@ New-Item -Path $modulePath -ItemType Directory | Out-Null
 
 New-ModuleManifest -Path (Join-Path $modulePath "$moduleName.psd1") -Guid (New-Guid) -ModuleVersion 1.0 -Author "Mark Birkedal Stjerslev Jakobsen" -CompanyName "N/A" -Description "Service test resource to demonstrate custom resources" -RootModule "$moduleName.psm1" -DscResourcesToExport "TestServiceClass" -Verbose
 
-#Copy module with class to module foldes with manifest
+#Copy module with class and help to module foldes with manifest
 Copy-Item (Join-Path $PSScriptRoot "$moduleName.psm1") -Destination $modulePath
+Copy-Item (Join-Path $PSScriptRoot "about_$moduleName.help.txt") -Destination $modulePath
