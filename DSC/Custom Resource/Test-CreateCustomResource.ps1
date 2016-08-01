73b470a24809c56f24b9785bee7686be6ebf224b
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2016-08-01 13:51:30 +0200

    Updated basic custom resource

--- DSC/Custom Resource/Test-CreateCustomResource.ps1
+++ DSC/Custom Resource/Test-CreateCustomResource.ps1
@@ -1,5 +1,5 @@
-$resourceName = "TestService"
-$modulePath = Join-Path $PSScriptRoot $resourceName
+$moduleName = "TestDSCModule"
+$modulePath = Join-Path $PSScriptRoot $moduleName
 
 if (Test-Path $modulePath -PathType Container) {
     Remove-Item $modulePath -Recurse -Force
@@ -7,19 +7,10 @@ if (Test-Path $modulePath -PathType Container) {
 
 New-Item -Path $modulePath -ItemType Directory | Out-Null
 
-New-xDscResource -Name $resourceName -Path $modulePath -Property $(
+New-xDscResource -Name "TestService" -Path $modulePath -Property $(
     New-xDscResourceProperty -Name Name -Type String -Attribute Key
     New-xDscResourceProperty -Name Status -Type String -Attribute Write -ValidateSet "Running","Stopped"
     New-xDscResourceProperty -Name Ensure -Type String -Attribute Write -ValidateSet "Present","Absent"
 ) -Force -Verbose
 
-New-ModuleManifest -Path (Join-Path $modulePath "$resourceName.psd1") -Guid (New-Guid) -ModuleVersion 1.0 -Author "Mark Jakobsen" -Description "Service test resource to demonstrate custom resources" -RootModule "$resourceName.psm1" -Verbose
-
-
-$moduleDestination = ($env:PSModulePath.Split(";") | Where-Object { $_ -match $env:USERNAME })
-
-if (Test-Path $moduleDestination -PathType Container) {
-    Remove-Item $moduleDestination -Recurse -Force
-}
-
-Copy-Item $modulePath -Destination $moduleDestination -Recurse -Force
+New-ModuleManifest -Path (Join-Path $modulePath "TestService.psd1") -Guid (New-Guid) -ModuleVersion 1.0 -Author "Mark Birkedal Stjerslev Jakobsen" -CompanyName "N/A" -Description "Service test resource to demonstrate custom resources" -RootModule "$moduleName.psm1" -Verbose
