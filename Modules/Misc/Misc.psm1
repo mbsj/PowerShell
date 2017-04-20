ff6ee37100c2bc91c8f998a3c372154a4939cdf7
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2017-04-20 09:05:50 +0200

    Corrected parametersets in Get-Password and updated formating.

--- Modules/Misc/Misc.psm1
+++ Modules/Misc/Misc.psm1
@@ -14,26 +14,24 @@
    Creates 4 numeric passwords with a length of 10 characters
 #>
 function Get-Password() {
-    [CmdletBinding(ConfirmImpact='Low')]
+    [CmdletBinding(DefaultParameterSetName = "Numeric", ConfirmImpact = 'Low')]
     param(
         # The number of characters in the password
-        [Parameter(Position=0, ParameterSetName="Normal")]
-        [Parameter(Position=0, ParameterSetName="Numeric")]
-        [Parameter(Position=0, ParameterSetName="Complex")]
+        [Parameter(Position = 0, ParameterSetName = "Numeric")]
+        [Parameter(Position = 0, ParameterSetName = "Complex")]
         [int]$Length = 16,
 
         # The number of passwords to generate
-        [Parameter(Position=1, ParameterSetName="Normal")]
-        [Parameter(Position=1, ParameterSetName="Numeric")]
-        [Parameter(Position=1, ParameterSetName="Complex")]
+        [Parameter(Position = 1, ParameterSetName = "Numeric")]
+        [Parameter(Position = 1, ParameterSetName = "Complex")]
         [int]$Count = 1,
 
         # Whether the password should be numeric
-        [Parameter(ParameterSetName="Numeric")]
+        [Parameter(ParameterSetName = "Numeric")]
         [Switch]$Numeric,
 
         # Whether the password should use special characters
-        [Parameter(ParameterSetName="Complex")]
+        [Parameter(ParameterSetName = "Complex")]
         [Switch]$Complex
 
     )
@@ -42,7 +40,8 @@ function Get-Password() {
 
     if ($Numeric) {
         $chars = @("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-    } else {
+    }
+    else {
         $chars = @("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
     }
 
@@ -77,7 +76,7 @@ function Get-Password() {
    Signs the script C:\Temp\Get-Output.ps1
 #>
 function Set-ScriptCertificate {
-    [CmdletBinding(ConfirmImpact='Low')]
+    [CmdletBinding(ConfirmImpact = 'Low')]
     param (
         # The path to the script which should be signed
         [Parameter(Mandatory = $true)]
@@ -92,7 +91,8 @@ function Set-ScriptCertificate {
     if ($codeSigningCerts.Count -lt 1) {
         Write-Host "No code signing certificates found. At least one is required." -ForegroundColor Red
         return
-    } elseif ($codeSigningCerts.Count -gt 1) {
+    }
+    elseif ($codeSigningCerts.Count -gt 1) {
         Write-Host "More than one certificate found. Please select one:" -ForegroundColor Green
 
         for ($i = 0; $i -lt $codeSigningCerts.Count; $i++) {
@@ -104,8 +104,9 @@ function Set-ScriptCertificate {
             $selected = Read-Host "Please select one" 
         } until ($selected -match "^[0-9]*$" -and $selected -le $codeSigningCerts.Count -and $selected -gt 0)
 
-        $cert = $codeSigningCerts[$selected -1]
-    } else {
+        $cert = $codeSigningCerts[$selected - 1]
+    }
+    else {
         $cert = $codeSigningCerts[0]
     }
 
@@ -114,9 +115,9 @@ function Set-ScriptCertificate {
 
 
 function Suspend-ScreenSaver {
-    [CmdletBinding(ConfirmImpact='Low')]
+    [CmdletBinding(ConfirmImpact = 'Low')]
     param(
-        [Parameter(Position=0)]
+        [Parameter(Position = 0)]
         [int]$Delay = 60
     )
 
@@ -130,9 +131,9 @@ function Suspend-ScreenSaver {
 }
 
 function Get-PasswordSecureString {
-    [CmdletBinding(ConfirmImpact='Low')]
+    [CmdletBinding(ConfirmImpact = 'Low')]
     param(
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory = $true, Position = 0)]
         [String]$PlainTextPassword
     )
 
