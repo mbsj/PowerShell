fd1a629ac48cd4ce45111866564ae0070c8b8bea
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2017-04-21 14:34:26 +0200

    Convertet Start-WsusCleanUp to advanced function

--- Scripts/Start-WsusCleanUp.ps1
+++ Scripts/Start-WsusCleanUp.ps1
@@ -1,14 +1,73 @@
-﻿[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null 
+﻿<#
+.SYNOPSIS
+    Starts a clenaup of the WSUS service on the default WSUS server
+.DESCRIPTION
+    Runs locally on the WSUS server or on a client with WSUS admin tools installed 
+    and triggers a cleanup job based on the configuration provided. 
 
-$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer(); 
+    Possible cleanups to run are one or more of the floowing: 
+        CleanupObsoleteComputers
+        CleanupObsoleteUpdates
+        CleanupUnneededContentFiles
+        CompressUpdates
+        DeclineExpiredUpdates
+        DeclineSupersededUpdates
+.EXAMPLE
+    .\Start-WsusCleanUp.ps1 
 
-$cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope; 
-$cleanupScope.DeclineSupersededUpdates = $true
-$cleanupScope.DeclineExpiredUpdates = $true
-$cleanupScope.CleanupUnneededContentFiles = $true
-$cleanupScope.CleanupObsoleteComputers = $true
-$cleanupScope.CleanupObsoleteUpdates     = $true 
-#$cleanupScope.CompressUpdates = $true 
+    Starts a cleanup with the default scope of DeclineSupersededUpdates, DeclineExpiredUpdates, CleanupUnneededContentFiles and CleanupObsoleteUpdates
+.EXAMPLE
+    .\Start-WsusCleanUp.ps1 -CleanupScope "DeclineSupersededUpdates", "DeclineExpiredUpdates"
 
-$cleanupManager = $wsus.GetCleanupManager(); 
-$cleanupManager.PerformCleanup($cleanupScope); 
+    Starts a cleanup, only running the declining cleanup scopes
+.EXAMPLE
+    .\Start-WsusCleanUp.ps1 -CleanupScope "*"
+
+    Starts a cleanup with all possible cleanup scopes
+#>
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+Param (
+    # Type of cleanup to run
+    [Parameter(Mandatory = $false, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet("DeclineSupersededUpdates", "DeclineExpiredUpdates", "CleanupUnneededContentFiles", "CleanupObsoleteComputers", "CleanupObsoleteUpdates", "CompressUpdates", "*")]
+    [String[]]$CleanupScope = @("DeclineSupersededUpdates", "DeclineExpiredUpdates", "CleanupUnneededContentFiles", "CleanupObsoleteUpdates")
+)
+    
+begin {
+    [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null 
+
+    $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer(); 
+
+    if (-not $wsus) {
+        throw "WSUS service was not found"
+    }
+
+    if ($CleanupScope -eq "*") {
+        $CleanupScope = @("DeclineSupersededUpdates", "DeclineExpiredUpdates", "CleanupUnneededContentFiles", "CleanupObsoleteComputers", "CleanupObsoleteUpdates", "CompressUpdates")
+    }
+}
+    
+process {
+    $cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope; 
+
+    $CleanupScope | ForEach-Object {
+        switch ($_) {
+            "CleanupObsoleteComputers" { $cleanupScope.CleanupObsoleteComputers = $true } 
+            "CleanupObsoleteUpdates" { $cleanupScope.CleanupObsoleteUpdates = $true }
+            "CleanupUnneededContentFiles" { $cleanupScope.CleanupUnneededContentFiles = $true } 
+            "CompressUpdates" { $cleanupScope.CompressUpdates = $true }
+            "DeclineExpiredUpdates" { $cleanupScope.DeclineExpiredUpdates = $true } 
+            "DeclineSupersededUpdates" { $cleanupScope.DeclineSupersededUpdates = $true } 
+            default { throw "Invalid cleanup scope"}
+        }
+    }
+
+    if ($pscmdlet.ShouldProcess("WSUS service", "Run cleanup")) {
+        $cleanupManager = $wsus.GetCleanupManager(); 
+        $cleanupManager.PerformCleanup($cleanupScope); 
+    }
+}
+    
+end {
+}
