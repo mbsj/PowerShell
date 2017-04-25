e91e298e729c9bbd1618787027e3d5dde9244b88
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2017-04-25 12:07:57 +0200

    Updated Set-LogonWallpaper to proper advanced function

--- Scripts/Set-LogonWallpaper.ps1
+++ Scripts/Set-LogonWallpaper.ps1
@@ -1,9 +1,35 @@
-﻿Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background -Name OEMBackground -Value 1 -Type DWord
+﻿<#
+.SYNOPSIS
+    Sets the logon wallpaper on Windows 7
+.DESCRIPTION
+    Sets a logon wallpaper on Windows 7 be exploiting the "Out Of Box Experience".
+    Sets the registry key for OEM background and places the specified image file in the OOBE backgrounds folder.
+.EXAMPLE
+    .\Set-LogonWallpaper.ps1 -ImagePath
+#>
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+Param (
+    # Type of cleanup to run
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript( {Test-Path -Path $_ -PathType Leaf})]
+    [String[]]$ImagePath
+)
 
-if (-not (Test-Path "C:\Windows\System32\oobe\info\backgrounds")) {
-    New-Item "C:\Windows\System32\oobe\info\backgrounds" -ItemType Directory -Force
+begin {
+    $regKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background"
+    $oemPath = "C:\Windows\System32\oobe\info\backgrounds"
 }
 
-if (Test-Path "$env:USERPROFILE\Pictures\LogonScreen.jpg") {
-    Copy-Item -Path "$env:USERPROFILE\Pictures\LogonScreen.jpg" -Destination "C:\Windows\System32\oobe\info\backgrounds\backgroundDefault.jpg" -Force
+process {
+    Set-ItemProperty -Path $regKey -Name "OEMBackground" -Value 1 -Type DWord -WhatIf:$WhatIfPreference
+
+    if (-not (Test-Path $oemPath)) {
+        New-Item $oemPath -ItemType Directory -Force -WhatIf:$WhatIfPreference
+    }
+
+    Copy-Item -Path $ImagePath -Destination (Join-Path -Path $oemPath -ChildPath "backgroundDefault.jpg") -Force -WhatIf:$WhatIfPreference
+}
+
+end {
 }
