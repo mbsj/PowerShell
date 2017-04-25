9f9903fb5cdfeee8edbf5251c01be405a1b0a9b4
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2017-04-25 09:54:53 +0200

    Updated Invoke-PesterBuild to create report pr. test and generate code coverage

--- Invoke-PesterBuild.ps1
+++ Invoke-PesterBuild.ps1
@@ -2,6 +2,7 @@ param(
     [string]$SourceDir = $env:BUILD_SOURCESDIRECTORY,
     [string]$TempDir = $env:TEMP
 )
+
 $ErrorActionPreference = "Stop"
  
 $modulePath = Join-Path $TempDir Pester-master\Pester.psm1
@@ -18,7 +19,25 @@ if (-not(Test-Path $modulePath)) {
 }
  
 Import-Module $modulePath -DisableNameChecking
- 
-$outputFile = Join-Path $SourceDir "TEST-pester.xml"
- 
-Invoke-Pester -Path $SourceDir -PassThru -OutputFile $outputFile -OutputFormat NUnitXml -EnableExit
+
+Get-ChildItem $SourceDir -Include "*.Tests.ps1" -Recurse | ForEach-Object {
+    $outputFile = Join-Path $SourceDir "TEST-pester_$($_.Name -replace "\.Tests",'').xml"
+    $coverageFile = $_.FullName -replace ".Tests", ''
+
+    $params = @{
+        Script = $_.FullName
+        PassThru = $true
+        OutputFile = $outputFile
+        OutputFormat = "NUnitXml"
+        EnableExit = $true
+    }
+
+    if (Test-Path $coverageFile) {
+        $params.Add("CodeCoverage", $coverageFile)
+    } elseif (Test-Path ($coverageFile -replace "ps1", "psm1")) {
+        $coverageFile = $coverageFile -replace "ps1", "psm1"
+        $params.Add("CodeCoverage", $coverageFile)
+    } 
+
+    Invoke-Pester @params
+}
