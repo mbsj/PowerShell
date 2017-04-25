4fa5e2dacb556033b66547baf3c07c80090e6fba
Mark Birkedal Stjerslev Jakobsen <mark.jakobsen@outlook.com>
2017-04-25 12:34:11 +0200

    Added tests for Set-LogonWallpaper

---
+++ Scripts/Set-LogonWallpaper.Tests.ps1
@@ -0,0 +1,38 @@
+$scriptFile = Join-Path -Path $PSScriptRoot -ChildPath "Set-LogonWallpaper.ps1"
+
+$regKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background"
+$oemPath = "C:\Windows\System32\oobe\info\backgrounds"
+$imagePath = "TestDrive:\wallpaper.png"
+
+Describe "Set-LogonWallpaper" {
+    Mock Set-ItemProperty {}
+    Mock Remove-Item {}
+    Mock New-Item {}
+    Mock Copy-Item {}
+
+    Set-Content -Value "" -Path $imagePath
+
+    It "Should run without error" {
+        $scriptRun = { & $scriptFile -ImagePath $imagePath -Force }
+
+        $scriptRun | Should Not Throw
+    }
+
+    It "Sets OEMBackground registry key" {
+        Assert-MockCalled -CommandName Set-ItemProperty -Times 1
+    }
+
+    It "Removes OEMBackground folder due to -Force" {
+        Assert-MockCalled -CommandName Remove-Item -Times 1
+    }
+
+    It "Creates new OEMBackground folder" {
+        Assert-MockCalled -CommandName New-Item -Times 1
+    }
+
+    it "Copies image file to OEMBackground folder" {
+        Assert-MockCalled -CommandName Copy-Item -Times 1 -ParameterFilter { 
+            $Path -eq $imagePath
+        }
+    }
+}
