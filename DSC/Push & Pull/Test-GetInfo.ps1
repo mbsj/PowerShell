d98cf8b7cc780ebf11161e96c145a919c2c3d805
Jakobsen.Mark Birkedal Stjerslev MVJ <MVJ@kmd.dk>
2017-05-17 12:04:19 +0200

    Removed old DSC test files

--- DSC/Push & Pull/Test-GetInfo.ps1
+++
@@ -1,7 +0,0 @@
-Get-DscConfigurationStatus -CimSession "MARK-VM" -All
-
-# Install-Module PowerShellCookbook
-Show-Object -InputObject (Get-DscConfigurationStatus -CimSession "MARK-VM")
-
-# Install-Module xDscDiagnostics
-Get-xDscOperation -ComputerName "MARK-VM"
