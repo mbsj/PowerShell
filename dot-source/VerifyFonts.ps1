5eaadd230e0720d94b0029fcc64526e22238011d
Mark Birkedal Stjerslev (MVJ) <MVJ@kmd.dk>
2019-08-02 12:08:47 +0200

    added code to install missing fonts

--- dot-source/VerifyFonts.ps1
+++ dot-source/VerifyFonts.ps1
@@ -1,15 +1,37 @@
-$requiredFonts = @(
-    "Fira Code Bold (TrueType)",
-    "Fira Code Medium (TrueType)",
-    "Fira Code Retina (TrueType)",
-    "Fira Code Light (TrueType)",
-    "Fira Code Regular (TrueType)"
-)
+$fontFilesPathRoot = Join-Path -Path $env:OneDriveConsumer -ChildPath "Technical\FiraCode_1.207\ttf"
+
+$requiredFonts = @{
+    "Fira Code Bold (TrueType)" = "FiraCode-Bold.ttf";
+    "Fira Code Medium (TrueType)" = "FiraCode-Medium.ttf";
+    "Fira Code Retina (TrueType)" = "FiraCode-Retina.ttf";
+    "Fira Code Light (TrueType)" = "FiraCode-Light.ttf";
+    "Fira Code Regular (TrueType)" = "FiraCode-Regular.ttf"
+}
 
 $installedFonts = Get-Font
 
-$requiredFonts | ForEach-Object {
+$missingFonts = @()
+$requiredFonts.Keys | ForEach-Object {
     if ($installedFonts.Keys -notcontains $_) {
-        Write-Warning "Font $_ missing. Download and install from https://github.com/tonsky/FiraCode/releases/latest"
+        Write-Warning "Font `"$_`" not installed."
+        $missingFonts += $_
+    }
+}
+
+if ($missingFonts) {
+    Write-Warning "To install missing fonts, use the function `"Install-MissingFont`""
+}
+
+function Install-MissingFont {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+    )
+
+    process {
+        $missingFonts | ForEach-Object {
+            Write-Verbose "Installing missing font `"$_`""
+            $fontFilePath = Join-Path -Path $fontFilesPathRoot -ChildPath $requiredFonts[$_]
+            Install-Font -Path $fontFilePath -Verbose:$VerbosePreference -WhatIf:$WhatIfPreference
+        }
     }
 }
