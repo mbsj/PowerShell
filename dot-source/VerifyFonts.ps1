9f5360074eadaf7e4c78e048d92d03724cae3647
Mark Birkedal Stjerslev (MVJ) <MVJ@kmd.dk>
2019-08-05 11:18:44 +0200

    Added verbose output

--- dot-source/VerifyFonts.ps1
+++ dot-source/VerifyFonts.ps1
@@ -1,15 +1,32 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+)
+
+Write-Verbose "Verifying that fonts are installed..."
 $fontFilesPathRoot = Join-Path -Path $env:OneDriveConsumer -ChildPath "Technical\FiraCode_1.207\ttf"
 
 $requiredFonts = @{
-    "Fira Code Bold (TrueType)" = "FiraCode-Bold.ttf";
-    "Fira Code Medium (TrueType)" = "FiraCode-Medium.ttf";
-    "Fira Code Retina (TrueType)" = "FiraCode-Retina.ttf";
-    "Fira Code Light (TrueType)" = "FiraCode-Light.ttf";
+    "Fira Code Bold (TrueType)"    = "FiraCode-Bold.ttf";
+    "Fira Code Medium (TrueType)"  = "FiraCode-Medium.ttf";
+    "Fira Code Retina (TrueType)"  = "FiraCode-Retina.ttf";
+    "Fira Code Light (TrueType)"   = "FiraCode-Light.ttf";
     "Fira Code Regular (TrueType)" = "FiraCode-Regular.ttf"
 }
 
+Write-Verbose "The following fonts are expected to be installed:"
+$requiredFonts.Keys | Sort-Object | ForEach-Object {
+    Write-Verbose "`t$_"
+}
+
+Write-Verbose "Getting installed fonts..."
 $installedFonts = Get-Font
 
+Write-Verbose "The following fonts are installed:"
+$installedFonts.Keys | Sort-Object | ForEach-Object {
+    Write-Verbose "`t$_"
+}
+
+Write-Verbose "Checking for missing fonts..."
 $missingFonts = @()
 $requiredFonts.Keys | ForEach-Object {
     if ($installedFonts.Keys -notcontains $_) {
@@ -20,6 +37,9 @@ $requiredFonts.Keys | ForEach-Object {
 
 if ($missingFonts) {
     Write-Warning "To install missing fonts, use the function `"Install-MissingFont`""
+}
+else {
+    Write-Verbose "No missing fonts."
 }
 
 function Install-MissingFont {
