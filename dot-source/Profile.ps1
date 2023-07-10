0e1d7da6a51a4b43e6aee5ddadc91de7d2e8987d
Mark Birkedal Stjerslev <mvj@kmd.dk>
2023-07-10 09:44:37 +0200

    Removed redundant functions and updated Oh My Posh integration

--- dot-source/Profile.ps1
+++ dot-source/Profile.ps1
@@ -3,29 +3,11 @@ param()
 
 Set-StrictMode -Version Latest
 
-function prompt {
-    $Host.UI.RawUI.WindowTitle = $Env:USERNAME + " - " + $Env:COMPUTERNAME + " - " + $Host.Name + " - " + $Host.Version
-    "PS " + $(get-location) + ">"
-}
-
 $env:PSModulePath += ";C:\Users\$env:USERNAME\OneDrive\PowerShell\Modules"
 $env:PSModulePath += ";C:\Users\$env:USERNAME\OneDrive - KMD\PowerShell_KMD\Modules"
 
-$modulesToImport = @(
-    "posh-git",
-    "oh-my-posh"
-)
-
-$modulesToImport | ForEach-Object {
-    if (Get-Module -ListAvailable $_) {
-        Import-Module -Name $_
-    }
-}
-
-if (Get-Module -ListAvailable "oh-my-posh") {
-    Set-PoshPrompt Agnoster
-}
-
-if (Get-Module -ListAvailable "PowerShellCookbook") {
-    Import-Module PowerShellCookbook -Prefix "Cookbook"
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+    oh-my-posh init pwsh --config "C:\Users\$env:USERNAME\AppData\Local\Programs\oh-my-posh\themes\kushal.omp.json" | Invoke-Expression
+} else {
+    Write-Warning "Oh My Posh is not installed. Use the following command to install it: `r`n`twinget install JanDeDobbeleer.OhMyPosh -s winget"
 }
